a911698d039767fa4320200c2afa26042b428e0c
sushidesu <hugemerge@gmail.com>
2022-01-04 15:55:45 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,60 @@
+import { useFilterSelect } from "./lib/use-filter-select"
+
 export const App = () => {
-  return <div>hello</div>
+  const [selects, values] = useFilterSelect({
+    children: [
+      {
+        label: "札幌市",
+        value: "sapporo",
+        children: [
+          {
+            label: "北区",
+            value: "kitaku",
+            children: []
+          },
+          {
+            label: "南区",
+            value: "minamiku",
+            children: []
+          },
+          {
+            label: "東区",
+            value: "higashiku",
+            children: []
+          }
+        ]
+      },
+      {
+        label: "小樽市",
+        value: "otarushi",
+        children: [
+          {
+            label: "銭函",
+            value: "zenibako",
+            children: []
+          },
+          {
+            label: "オタモイ",
+            value: "otamoi",
+            children: []
+          }
+        ]
+      }
+    ]
+  })
+
+  console.log(values)
+
+  return (
+    <div>
+      <div>hello</div>
+      {selects.map(({ options, ...props }, i) => (
+        <select key={i} {...props}>{
+          options.map(opt => (
+            <option key={opt.value} value={opt.value}>{opt.label}</option>
+          ))
+        }</select>
+      ))}
+    </div>
+  )
 }
---
+++ src/lib/use-filter-select.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react"
+
+type FilterNodeRoot = {
+  children: FilterNode[]
+}
+
+type FilterNode = {
+  label: string
+  value: string
+  children: FilterNode[]
+}
+
+type OptionItem = {
+  label: string
+  value: string
+}
+
+type SelectProps = {
+  value: string | undefined
+  onChange: React.ChangeEventHandler<HTMLSelectElement>
+  options: OptionItem[]
+}
+
+export const useFilterSelect = (tree: FilterNodeRoot): [SelectProps[], {
+  layer01: OptionItem | undefined,
+  layer02: OptionItem | undefined,
+  layer03: OptionItem | undefined
+}] => {
+  const layer01_default = tree.children[0]
+  const layer02_default = layer01_default?.children[0]
+  const layer03_default = layer02_default?.children[0]
+
+  const [layer01, setLayer01] = useState<string | undefined>(layer01_default?.value)
+  const [layer02, setLayer02] = useState<string | undefined>(layer02_default?.value)
+  const [layer03, setLayer03] = useState<string | undefined>(layer03_default?.value)
+
+  const l1 = tree.children.find((node) => node.value === layer01)
+  const l2 = l1?.children.find((node) => node.value === layer02)
+  const l3 = l2?.children.find((node) => node.value === layer03)
+
+  return [
+  [
+    {
+      options: tree.children.map(node => _convert(node)),
+      value: layer01,
+      onChange: (e) => {
+        setLayer01(e.target.value)
+      }
+    },
+    {
+      options: l1?.children.map((node) => _convert(node)) ?? [],
+      value: layer02,
+      onChange: (e) => {
+        setLayer02(e.target.value)
+      }
+    },
+    {
+      options: l2?.children.map((node) => _convert(node)) ?? [],
+      value: layer03,
+      onChange: (e) => {
+        setLayer03(e.target.value)
+      }
+    }
+  ],
+  {
+    layer01: l1,
+    layer02: l2,
+    layer03: l3
+  }]
+}
+
+const _convert = (node: FilterNode): OptionItem => {
+  return {
+    value: node.value,
+    label: node.label
+  }
+}
